2e67f211ec812a3a21108bb0b9b84fc79f8efb60
Matt Holloway <hollowmatt@gmail.com>
2025-10-03 13:06:49 +0100

    add GraphCanvas from Reagraph

--- ch2/src/App.jsx
+++ ch2/src/App.jsx
@@ -1,11 +1,14 @@
+import { GraphCanvas } from 'reagraph';
 import './App.css'
 
 function App() {
+  
+
   return (
     <>
       <Menu />
-      <div className='content'>
-        <p>Content will go here</p>
+      <div className='canvas'>
+        <MyDiagram/>
       </div>
       <p className="docs">
         Read the docs to learn more.
@@ -32,6 +35,21 @@ function MenuItem({name, href, ...rest}) {
     <li className='menu-item'>
       <a className='menu-link' href={href} {...rest}> {name} </a>
     </li> 
+  )
+}
+
+function MyDiagram () {
+  const nodes = [
+    { id: '1', label: 'Node 1', icon: 'https://reagraph.com/favicon.ico' },
+    { id: '2', label: 'Node 2' }
+  ];
+
+  const edges = [
+    { source: '1', target: '2', id: '1-2', label: '1-2' }, 
+    { source: '2', target: '1', id: '2-1', label: '2-1' }
+  ];
+  return (
+    <GraphCanvas nodes={nodes} edges={edges} />
   )
 }
 
